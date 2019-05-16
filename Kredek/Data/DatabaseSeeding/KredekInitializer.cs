9f5202a7c8c0d70d57ba086d010f13ba1076a8b9
Anton Vlasiuk <vlasiuk.anton@gmail.com>
2019-05-17 00:34:26 +0200

    [Added] base pages inititalizattion and its translations

--- Kredek/Data/DatabaseSeeding/KredekInitializer.cs
+++ Kredek/Data/DatabaseSeeding/KredekInitializer.cs
@@ -17,8 +17,31 @@ namespace Kredek.Data.DatabaseSeeding
         private readonly string HomeNavigationTabNameEn = "Home";
         private readonly string HomeNavigationTabNamePl = "Główna";
         private readonly string HomePageName = GlobalVariables.HomePageName;
+        private readonly int HomePageNavigationIndex = 1;
 
         #endregion Home page
+
+        #region Team page
+
+        private const string TeamNavigationTabNameEn = "Team";
+        private const string TeamNavigationTabNamePl = "Zespół";
+        private const string TeamPageTitleEn = "Team";
+        private const string TeamPageTitlePl = "Zespół";
+        private readonly string TeamPageName = "Team";
+        private readonly int TeamPageNavigationIndex = 2;
+
+        #endregion Team page
+
+        #region Course page
+
+        private const string CourseNavigationTabNameEn = "Course";
+        private const string CourseNavigationTabNamePl = "Kurs";
+        private const string CoursePageTitleEn = "Course";
+        private const string CoursePageTitlePl = "Kurs";
+        private readonly string CoursePageName = "Course";
+        private readonly int CoursePageNavigationIndex = 3;
+
+        #endregion Course page
 
         #region Blog page
 
@@ -27,8 +50,31 @@ namespace Kredek.Data.DatabaseSeeding
         private const string BlogPageTitleEn = "Blog";
         private const string BlogPageTitlePl = "Blog";
         private readonly string BlogPageName = "Blog";
+        private readonly int BlogPageNavigationIndex = 4;
 
         #endregion Blog page
+
+        #region About page
+
+        private const string AboutNavigationTabNameEn = "About";
+        private const string AboutNavigationTabNamePl = "O Nas";
+        private const string AboutPageTitleEn = "About";
+        private const string AboutPageTitlePl = "O Nas";
+        private readonly string AboutPageName = "About";
+        private readonly int AboutPageNavigationIndex = 5;
+
+        #endregion About page
+
+        #region Contact page
+
+        private const string ContactNavigationTabNameEn = "Contact";
+        private const string ContactNavigationTabNamePl = "Kontakt";
+        private const string ContactPageTitleEn = "Contact";
+        private const string ContactPageTitlePl = "Kontakt";
+        private readonly string ContactPageName = "Contact";
+        private readonly int ContactPageNavigationIndex = 6;
+
+        #endregion Contact page
 
         private readonly ApplicationDbContext _context;
 
@@ -49,12 +95,13 @@ namespace Kredek.Data.DatabaseSeeding
             _context.SaveChanges();
         }
 
-        public void CreateANewPage(string name, bool isActive = true)
+        public void CreateANewPage(string name, int navigationIndex, bool isActive = true)
         {
             var newPage = new WebsitePage()
             {
                 Name = name,
                 IsActive = isActive,
+                NavigationIndex = navigationIndex
             };
 
             _context.WebsitePages.Add(newPage);
@@ -83,8 +130,12 @@ namespace Kredek.Data.DatabaseSeeding
 
         public void CreateDefaultPages()
         {
-            CreateANewPage(GlobalVariables.HomePageName, GlobalVariables.HomePageIsActive);
-            CreateANewPage(BlogPageName);
+            CreateANewPage(GlobalVariables.HomePageName, HomePageNavigationIndex, GlobalVariables.HomePageIsActive);
+            CreateANewPage(BlogPageName, BlogPageNavigationIndex);
+            CreateANewPage(AboutPageName, AboutPageNavigationIndex);
+            CreateANewPage(TeamPageName, TeamPageNavigationIndex);
+            CreateANewPage(ContactPageName, ContactPageNavigationIndex);
+            CreateANewPage(CoursePageName, CoursePageNavigationIndex);
         }
 
         public void CreateDefaultPagesTranslations()
@@ -96,15 +147,28 @@ namespace Kredek.Data.DatabaseSeeding
 
             //Blog page translations
             CreatePagePlAndEnTranslations(BlogPageName, languages, BlogPageTitlePl, BlogPageTitleEn, BlogNavigationTabNamePl, BlogNavigationTabNameEn);
+            //About page translations
+            CreatePagePlAndEnTranslations(AboutPageName, languages, AboutPageTitlePl, AboutPageTitleEn, AboutNavigationTabNamePl, AboutNavigationTabNameEn);
+
+            //TeamPageName page translations
+            CreatePagePlAndEnTranslations(TeamPageName, languages, TeamPageTitlePl, TeamPageTitleEn, TeamNavigationTabNamePl, TeamNavigationTabNameEn);
+
+            //Contact page translations
+            CreatePagePlAndEnTranslations(ContactPageName, languages, ContactPageTitlePl, ContactPageTitleEn, ContactNavigationTabNamePl, ContactNavigationTabNameEn);
+
+            //Course page translations
+            CreatePagePlAndEnTranslations(CoursePageName, languages, CoursePageTitlePl, CoursePageTitleEn, CourseNavigationTabNamePl, CourseNavigationTabNameEn);
         }
 
         /// <summary>
         /// Hardcode method to create Polish and English page translations
         /// </summary>
+        /// <param name="pageName"></param>
         /// <param name="languages"></param>
         /// <param name="pageTitlePl"></param>
         /// <param name="pageTitleEn"></param>
-        /// <param name="pageName"></param>
+        /// <param name="navigationTabNamePl"></param>
+        /// <param name="navigationTabNameEn"></param>
         private void CreatePagePlAndEnTranslations(string pageName, IList<Language> languages, string pageTitlePl, string pageTitleEn, string navigationTabNamePl, string navigationTabNameEn)
         {
             var page = _context.WebsitePages.Single(x => x.Name == pageName);
