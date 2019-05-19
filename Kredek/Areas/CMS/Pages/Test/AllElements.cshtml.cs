e05ca1c7dd85d075acaacf1ab65a17f4e21b3881
Anton Vlasiuk <vlasiuk.anton@gmail.com>
2019-05-19 16:48:30 +0200

    Update AllElements.cshtml.cs

--- Kredek/Areas/CMS/Pages/Test/AllElements.cshtml.cs
+++ Kredek/Areas/CMS/Pages/Test/AllElements.cshtml.cs
@@ -1,11 +1,7 @@
 ﻿using Kredek.Data.Models;
-using Kredek.Data.Models.ContentElementTranslationTemplates;
-using Kredek.Data.ViewModels;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Internal;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace Kredek.Areas.CMS.Pages.Test
@@ -16,7 +12,6 @@ namespace Kredek.Areas.CMS.Pages.Test
 
         public string LanguageName { get; private set; } = "Polski";
         public IList<ContentElement> PageContent { get; set; }
-        public PageContentViewModel PageContentViewModel { get; set; }
 
         public AllElementsModel(Kredek.Data.ApplicationDbContext context)
         {
@@ -28,32 +23,6 @@ namespace Kredek.Areas.CMS.Pages.Test
             PageContent = await _context.ContentElement
                 .Include(x => x.ContentElementTranslations)
                 .ToListAsync();
-
-            CreateViewModel(pageName, PageContent[0].ContentElementTranslations.ToList());
-        }
-
-        private List<PageContentElementViewModel> CreatePageContentElementViewModel(List<ContentElementTranslation> elementsTranslations)
-        {
-            var elements = new List<PageContentElementViewModel>();
-
-            foreach (var translation in elementsTranslations)
-            {
-                elements.Add(new PageContentElementViewModel()
-                {
-                    TypeName = translation.GetType().ShortDisplayName()
-                });
-            }
-
-            return elements;
-        }
-
-        private void CreateViewModel(string pageName, List<ContentElementTranslation> elementsTranslations)
-        {
-            PageContentViewModel = new PageContentViewModel()
-            {
-                Elements = CreatePageContentElementViewModel(elementsTranslations),
-                PageName = pageName
-            };
         }
     }
 }
