0733252f3f51b5f16d39d4d66c93fceef136f073
Anton Vlasiuk <vlasiuk.anton@gmail.com>
2019-05-20 22:24:27 +0200

    [Save] added dropdown input with JS on item click

--- Kredek/Areas/CMS/Pages/Test/AllElements.cshtml.cs
+++ Kredek/Areas/CMS/Pages/Test/AllElements.cshtml.cs
@@ -1,5 +1,7 @@
 ﻿using Kredek.Data.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Threading.Tasks;
@@ -14,6 +16,9 @@ namespace Kredek.Areas.CMS.Pages.Test
         public IList<ContentElement> PageContent { get; set; }
         public string PageName { get; set; }
 
+        [BindProperty]
+        public int WebsitePageId { get; set; }
+
         public AllElementsModel(Kredek.Data.ApplicationDbContext context)
         {
             _context = context;
@@ -22,6 +27,20 @@ namespace Kredek.Areas.CMS.Pages.Test
         public async Task OnGetAsync(string pageName)
         {
             PageName = pageName;
+
+            await GetContent();
+        }
+
+        public async Task OnPost()
+        {
+            PageName = (await _context.WebsitePages.SingleAsync(x => x.WebsitePageId == WebsitePageId)).Name;
+
+            await GetContent();
+        }
+
+        private async Task GetContent()
+        {
+            ViewData["WebsitePageId"] = new SelectList(_context.WebsitePages, "WebsitePageId", "Name");
 
             PageContent = await _context.ContentElement
                 .Include(x => x.ContentElementTranslations)
