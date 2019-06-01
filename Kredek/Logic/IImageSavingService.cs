70f4e0f07fb5d0e6680d074080e8b0d3fc3807f7
Anton Vlasiuk <vlasiuk.anton@gmail.com>
2019-06-01 21:52:27 +0200

    Update IImageSavingService.cs

--- Kredek/Logic/IImageSavingService.cs
+++ Kredek/Logic/IImageSavingService.cs
@@ -4,6 +4,12 @@ namespace Kredek.Logic
 {
     public interface IImageSavingService
     {
+        /// <summary>
+        /// Saves image to static folder on server
+        /// </summary>
+        /// <param name="img"></param>
+        /// <param name="pageId"></param>
+        /// <returns>Path to static folder ( important: result does not contains path to wwwroot folder!) </returns>
         string SaveImage(IFormFile img, int pageId);
     }
 }
