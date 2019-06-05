057cc8a6f89e6407fe756ec2e357289ba45a03e9
Anton Vlasiuk <alealessso@gmail.com>
2019-06-05 20:38:43 +0200

    [Created] Team Model

---
+++ Kredek/Data/Models/ContentElementTranslationTemplates/TeamMember.cs
@@ -0,0 +1,9 @@
+﻿namespace Kredek.Data.Models.ContentElementTranslationTemplates
+{
+    public class TeamMember : ContentElementTranslation
+    {
+        public string DisplayName { get; set; }
+        public string ShortDescribtion { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
