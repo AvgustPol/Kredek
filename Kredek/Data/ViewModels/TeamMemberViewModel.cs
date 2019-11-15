cedd7308ab9b663e662dd0c08403547a185b243a
Anton Vlasiuk <alealessso@gmail.com>
2019-11-16 00:45:22 +0100

    Create TeamMemberViewModel.cs

---
+++ Kredek/Data/ViewModels/TeamMemberViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Kredek.Data.Models.ContentElementTranslationTemplates;
+using Kredek.Models.Common.Emuns.Foo;
+
+namespace Kredek.Data.ViewModels
+{
+    public class TeamMemberViewModel
+    {
+        public InputType InputType { get; set; }
+        public TeamMember TeamMember { get; set; }
+    }
+}
