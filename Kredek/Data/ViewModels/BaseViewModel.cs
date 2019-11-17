eb45a3742fa474fa6dadae29978299af095fada6
Anton Vlasiuk <alealessso@gmail.com>
2019-11-17 13:36:50 +0100

    working on generic creation

---
+++ Kredek/Data/ViewModels/BaseViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Kredek.Data.Models.ContentElementTranslationTemplates;
+using Kredek.Models.Common.Emuns.Foo;
+
+namespace Kredek.Data.ViewModels
+{
+    public class BaseViewModel
+    {
+        public InputType InputType { get; set; }
+        public ContentElementTranslation Component { get; set; }
+    }
+}
