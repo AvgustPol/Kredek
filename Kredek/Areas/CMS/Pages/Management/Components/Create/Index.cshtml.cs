656936250a2faa7608ae0c9a170defc1fd35a5a6
Anton Vlasiuk <alealessso@gmail.com>
2019-11-18 20:04:56 +0100

    [ Working ] on swapping to generic create view

--- Kredek/Areas/CMS/Pages/Management/Components/Create/Index.cshtml.cs
+++ Kredek/Areas/CMS/Pages/Management/Components/Create/Index.cshtml.cs
@@ -1,10 +1,11 @@
-using System;
 using System.Threading.Tasks;
 using Kredek.Data;
 using Kredek.Data.Models.ContentElementTranslationTemplates;
 using Kredek.Data.ViewModels;
+using Kredek.Models.Common.Emuns;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Kredek.Areas.CMS.Pages.Management.Components.Create
 {
@@ -12,15 +13,31 @@ namespace Kredek.Areas.CMS.Pages.Management.Components.Create
     public class IndexModel : PageModel
     {
         private readonly ApplicationDbContext _context;
+        public AvailableTemplates Type { get; set; }
+        public int WebsitePageId { get; set; }
+        public ContentElementTranslation NewElement { get; set; }
         public IndexModel(ApplicationDbContext context)
         {
             _context = context;
         }
 
-        public ContentElementTranslation NewElement { get; set; }
 
-        public IActionResult OnGet()
+        public IActionResult OnGet(int id, AvailableTemplates type)
         {
+            WebsitePageId = id;
+
+            switch (type)
+            {
+                case AvailableTemplates.Foo:
+                    NewElement = new FooViewModel();
+                    break;
+
+                case AvailableTemplates.Boo:
+                    NewElement = new BooViewModel();
+                    break;
+            }
+
+            ViewData["Languages"] = new SelectList(_context.Languages, "LanguageId", "Name");
             return Page();
         }
 
